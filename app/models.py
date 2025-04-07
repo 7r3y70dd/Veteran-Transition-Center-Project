d4e2f27f7df1d8cd34da2960cd5382ab93ad18a9
treytodd <trey.todd@gmail.com>
2025-04-07 04:27:21 -0700

    'add data' side of application almost complete.           next: clean initialization and easy visulaization

--- app/models.py
+++ app/models.py
@@ -6,68 +6,112 @@ from datetime import datetime
 from . import db  # Import the db instance from your app
 
 # Define a CustomModel with three integers and calculated field
-class PopulationsModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)  # Primary key
-    programOneNumberOfParticipants = db.Column(db.Integer, nullable=False)  # First integer value
-    programTwoNumberOfParticipants = db.Column(db.Integer, nullable=False)  # Second integer value
-    programThreeNumberOfParticipants = db.Column(db.Integer, nullable=False)  # Third integer value
-    programOnePayPerDiem = db.Column(db.Integer, nullable=False)
-    programTwoPayPerDiem = db.Column(db.Integer, nullable=False)
-    programThreePayPerDiem = db.Column(db.Integer, nullable=False)
-    grandTotal = db.Column(db.Float, default=0)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)  # Current date as timestamp
-
-
-    def __init__(self, value1, value2, value3):
-        # Initialize with the three values
-        self.programOneNumberOfParticipants = value1
-        self.programTwoNumberOfParticipants = value2
-        self.programThreeNumberOfParticipants = value3
-        # Automatically calculate the calculated_value from the three integers
-        rates = RatesModel.query.first()
-        self.programOnePayPerDiem = (self.programOneNumberOfParticipants * rates.rate_1)
-        self.programTwoPayPerDiem = (self.programTwoNumberOfParticipants * rates.rate_2)
-        self.programThreePayPerDiem = (self.programThreeNumberOfParticipants * rates.rate_3)
-        costs = CostsPerProgramModel.query.all()
-        self.grandTotal = self.programOnePayPerDiem + self.programTwoPayPerDiem + self.programThreePayPerDiem
-        value = self.grandTotal
-        for cost in costs:
-            value -= cost.salary / 365
-        self.grandTotal = value
-
-
-
-    # # Method to calculate the value (example: sum of the three integers)
-    # def calculate_value(self, program):
-    #     # Here, we simply sum the three values, but you can modify it to any formula.
-    #     if program == 1:
-    #         related_integer = RatesModel.query.first()
-    #         return self.value1 * related_integer.rate1
-    #     elif program == 1:
-    #         related_integer = RatesModel.query.first()
-    #         return self.value1 * related_integer.rate1
-
-    def __repr__(self):
-        return f"<CustomModel {self.id} | {self.value1}, {self.value2}, {self.value3}, {self.calculated_value}>"
-
-class RatesModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    # title = db.Column(db.String(100), nullable=False)
-    rate1 = db.Column(db.Integer, default=1)
-    rate2 = db.Column(db.Integer, default=1)
-    rate3 = db.Column(db.Integer, default=1)
-
-    def update_rates(self, rate_1=None, rate_2=None, rate_3=None):
-        if rate_1:
-            self.rate_1 = rate_1
-        if rate_2:
-            self.rate_2 = rate_2
-        if rate_3:
-            self.rate_3 = rate_3
-        self.last_updated = datetime.utcnow()  # Update the timestamp
-        db.session.commit()
+# class PopulationsModel(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)  # Primary key
+#     programOneNumberOfParticipants = db.Column(db.Integer, nullable=False)  # First integer value
+#     programTwoNumberOfParticipants = db.Column(db.Integer, nullable=False)  # Second integer value
+#     programThreeNumberOfParticipants = db.Column(db.Integer, nullable=False)  # Third integer value
+#     programOnePayPerDiem = db.Column(db.Integer, nullable=False)
+#     programTwoPayPerDiem = db.Column(db.Integer, nullable=False)
+#     programThreePayPerDiem = db.Column(db.Integer, nullable=False)
+#     grandTotal = db.Column(db.Float, default=0)
+#     date_created = db.Column(db.DateTime, default=datetime.utcnow)  # Current date as timestamp
+#
+#
+#     def __init__(self, value1, value2, value3):
+#         # Initialize with the three values
+#         self.programOneNumberOfParticipants = value1
+#         self.programTwoNumberOfParticipants = value2
+#         self.programThreeNumberOfParticipants = value3
+#         # Automatically calculate the calculated_value from the three integers
+#         rates = RatesModel.query.first()
+#         self.programOnePayPerDiem = (self.programOneNumberOfParticipants * rates.rate_1)
+#         self.programTwoPayPerDiem = (self.programTwoNumberOfParticipants * rates.rate_2)
+#         self.programThreePayPerDiem = (self.programThreeNumberOfParticipants * rates.rate_3)
+#         costs = CostsPerProgramModel.query.all()
+#         self.grandTotal = self.programOnePayPerDiem + self.programTwoPayPerDiem + self.programThreePayPerDiem
+#         value = self.grandTotal
+#         for cost in costs:
+#             value -= cost.salary / 365
+#         self.grandTotal = value
+#
+#
+#
+#     # # Method to calculate the value (example: sum of the three integers)
+#     # def calculate_value(self, program):
+#     #     # Here, we simply sum the three values, but you can modify it to any formula.
+#     #     if program == 1:
+#     #         related_integer = RatesModel.query.first()
+#     #         return self.value1 * related_integer.rate1
+#     #     elif program == 1:
+#     #         related_integer = RatesModel.query.first()
+#     #         return self.value1 * related_integer.rate1
+#
+#     def __repr__(self):
+#         return f"<CustomModel {self.id} | {self.value1}, {self.value2}, {self.value3}, {self.calculated_value}>"
+#
+# class RatesModel(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     # title = db.Column(db.String(100), nullable=False)
+#     rate1 = db.Column(db.Integer, default=1)
+#     rate2 = db.Column(db.Integer, default=1)
+#     rate3 = db.Column(db.Integer, default=1)
+#
+#     def update_rates(self, rate_1=None, rate_2=None, rate_3=None):
+#         if rate_1:
+#             self.rate_1 = rate_1
+#         if rate_2:
+#             self.rate_2 = rate_2
+#         if rate_3:
+#             self.rate_3 = rate_3
+#         self.last_updated = datetime.utcnow()
+#         db.session.commit()
 
 class CostsPerProgramModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    grouping = db.Column(db.Integer, nullable=False)
-    salary = db.Column(db.Integer, nullable=False)
+    program_id = db.Column(db.Integer, db.ForeignKey('program_model.id'), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    salary = db.Column(db.Float, nullable=False)
+
+    def __init__(self, program_name, cost, name):
+        program = ProgramModel.query.filter_by(name=program_name).first()
+        if not program:
+            raise ValueError(f"Program with name '{program_name}' not found.")
+        self.program_id = program.id
+        self.salary = cost
+        self.name = name
+
+
+
+class ProgramModel(db.Model):
+    __tablename__ = 'program_model'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    rate = db.Column(db.Float, nullable=False)
+
+    def __init__(self, program_name, rate):
+        self.name = program_name
+        self.rate = rate
+
+class DailyEntriesModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    number_of_participants = db.Column(db.Integer, nullable=False)
+    program_id = db.Column(db.Integer, db.ForeignKey('program_model.id'), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+
+    def __init__(self, number_of_participants, program_id):
+        self.number_of_participants = number_of_participants
+        self.program_id = program_id
+
+
+class TotalModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    grand_total = db.Column(db.Float, nullable=False)
+
+    def __init__(self, grand_total, comment):
+        self.grand_total = grand_total
+        self.comment = comment
+
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 # from .models import User
 from . import db
-from flask import Blueprint, render_template
+from flask import render_template, request, redirect, url_for, Blueprint
+from .models import CostsPerProgramModel, ProgramModel, DailyEntriesModel
 
 main = Blueprint('main', __name__)
 
@@ -10,9 +11,136 @@ def index():
 
 @main.route('/add_data')
 def add_data():
-    return "This is route one."
+    return render_template('add_data.html')
+
+@main.route('/add_delete_support_staff', methods=['GET', 'POST'])
+def add_delete_support_staff():
+    if request.method == 'POST':
+        # Get the data from the form
+        program_name = request.form['program_name']
+        employee_name = request.form['employee_name']
+        salary = float(request.form['salary'])
+        action = request.form['action']  # Action to determine if we're adding or deleting
+
+        # Fetch the program from the database
+        program = ProgramModel.query.filter_by(name=program_name).first()
+
+        if program:
+            if action == 'add':
+                # Add the support staff to the database
+                new_support_staff = CostsPerProgramModel(
+                    program_id=program.id,
+                    name=employee_name,
+                    salary=salary
+                )
+                db.session.add(new_support_staff)
+                db.session.commit()
+                return redirect(url_for('main.index'))
+
+            elif action == 'delete':
+                # Delete the staff from the database
+                staff_to_delete = CostsPerProgramModel.query.filter_by(
+                    program_id=program.id,
+                    name=employee_name,
+                    salary=salary
+                ).first()
+
+                if staff_to_delete:
+                    db.session.delete(staff_to_delete)
+                    db.session.commit()
+
+                return redirect(url_for('main.index'))
+
+        else:
+            return "Program not found", 404
+
+    # Render the form for GET request
+    return render_template('add_delete_support_staff.html')
 
 @main.route('/veiw_data')
 def view_data():
     return "This is route one."
 
+
+@main.route('/add_delete_program', methods=['GET', 'POST'])
+def add_delete_program():
+    if request.method == 'POST':
+        # Get the data from the form
+        program_name = request.form['program_name']
+        rate = float(request.form['rate'])
+        action = request.form['action']  # Action to determine if we're adding or deleting
+
+        if action == 'add':
+            # Create a new ProgramModel entry
+            new_program = ProgramModel(program_name=program_name, rate=rate)
+
+            # Add the new entry to the database
+            db.session.add(new_program)
+            db.session.commit()
+            return redirect(url_for('main.index'))  # Redirect to the index or another page
+
+        elif action == 'delete':
+            # Find the program to delete
+            program_to_delete = ProgramModel.query.filter_by(name=program_name, rate=rate).first()
+
+            if program_to_delete:
+                # Delete the program from the database
+                db.session.delete(program_to_delete)
+                db.session.commit()
+
+            return redirect(url_for('main.index'))  # Redirect after deletion
+
+    # If the request method is GET, render the form
+    return render_template('add_delete_program.html')
+@main.route('/add_daily_totals', methods=['GET', 'POST'])
+def add_daily_totals():
+    if request.method == 'POST':
+        # Get all the data from the form
+        entries = request.form.getlist('number_of_participants')  # Get all participant numbers
+        entry_date = request.form['date']
+
+        for i, program in enumerate(ProgramModel.query.all()):
+            number_of_participants = int(entries[i]) if entries[i] else 0
+            program_name = program.name
+
+            # Fetch the program from the database
+            program = ProgramModel.query.filter_by(name=program_name).first()
+
+            if program:
+                # Check if a daily entry already exists for this program on the given date
+                existing_entry = DailyEntriesModel.query.filter_by(
+                    program_id=program.id,
+                    date=entry_date
+                ).first()
+
+                if existing_entry:
+                    # If entry exists, update the number of participants
+                    existing_entry.number_of_participants = number_of_participants
+                    db.session.commit()
+                else:
+                    # If entry doesn't exist, create a new entry
+                    new_daily_entry = DailyEntriesModel(
+                        number_of_participants=number_of_participants,
+                        program_id=program.id,
+                        date=entry_date
+                    )
+                    db.session.add(new_daily_entry)
+                    db.session.commit()
+
+            else:
+                # If the program doesn't exist
+                return "Program not found", 404
+
+        # Redirect after adding or updating
+        return redirect(url_for('main.index'))
+
+    # If the request method is GET, render the form
+    programs = ProgramModel.query.all()
+    last_entries = {}
+
+    # Get the last entry for each program (not strictly necessary anymore)
+    for program in programs:
+        last_entry = DailyEntriesModel.query.filter_by(program_id=program.id).order_by(DailyEntriesModel.date.desc()).first()
+        last_entries[program.id] = last_entry.number_of_participants if last_entry else 0
+
+    return render_template('add_daily_totals.html', programs=programs, last_entries=last_entries)
